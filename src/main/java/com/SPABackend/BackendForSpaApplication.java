66aca65bbdd91a27865cc2f52a9785996f827ce1
SergeyPodgorny <serkonpod@gmail.com>
2022-04-17 07:12:40 +0300

    initial commit

---
+++ src/main/java/com/SPABackend/BackendForSpaApplication.java
@@ -0,0 +1,13 @@
+package com.SPABackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SPABackend/BackendForSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SPABackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
